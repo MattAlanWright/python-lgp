dbce9a3a1d8e36c7eb4c5501db1f09b7115206c6
MattAlanWright <46428602+MattAlanWright@users.noreply.github.com>
2020-09-08 12:18:01 -0300

    Enforce current wimpiness of the library

--- train.py
+++ train.py
@@ -23,6 +23,14 @@ def run(arguments):
     parser.add_argument('--env', dest='env', type=str, help='OpenAI environment', default="CartPole-v1")
     parser.add_argument('--statespace', dest='statespace', type=int, help='Length of flattened state space', default=4)
     args = parser.parse_args(arguments)
+    
+    if args.env != "CartPole-v0" && args.env != "CartPole-v1":
+        print("Woops! So far this module only works in the CartPole environment!")
+        return
+    
+    if args.statespace != 4:
+        print("Woops! So far this module only works in the CartPole environment with a statespace size of 4!")
+        return
 
     ConfigureProgram(num_inputs=args.statespace)
 
