c89edad41aa805e1b2569593d3a34bc3c453607c
MattAlanWright <46428602+MattAlanWright@users.noreply.github.com>
2020-09-08 09:29:22 -0300

    Removed redundant comments

--- train.py
+++ train.py
@@ -19,7 +19,6 @@ import argparse
 
 def run(arguments):
 
-    # Setup the command line parsing to read the environment title
     parser = argparse.ArgumentParser(description='Perform linear GP evolution for a given environment.')
     parser.add_argument('--env', dest='env', type=str, help='OpenAI environment', default="CartPole-v1")
     parser.add_argument('--statespace', dest='statespace', type=int, help='Length of flattened state space', default=4)
@@ -27,14 +26,10 @@ def run(arguments):
 
     ConfigureProgram(num_inputs=args.statespace)
 
-    # Get environment details
     env = gym.make(args.env)
-
-    # Create Trainer
     trainer = Trainer(env)
-
-    # Try to generate an agent
     trainer.evolve()
 
 if __name__ == "__main__":
     run(sys.argv[1:])
+    
