dafaece962f992182bb31541ea7222f4f359ddf6
MattAlanWright <46428602+MattAlanWright@users.noreply.github.com>
2020-09-10 13:33:21 -0300

    Configure Trainer through cmd line args

--- train.py
+++ train.py
@@ -20,11 +20,25 @@ import argparse
 def run(arguments):
 
     parser = argparse.ArgumentParser(description='Perform linear GP evolution for a given environment.')
+    
+    # Program configuration
     parser.add_argument('--minps', dest='min_prog_size', type=int, help='Minimum number of instructions per Program', default=32)
     parser.add_argument('--maxps', dest='max_prog_size', type=int, help='Maximum number of instructions per Program', default=1024)
     parser.add_argument('--padd', dest='padd', type=float, help='Instruction addition strength', default=0.7)
     parser.add_argument('--pdel', dest='pdel', type=float, help='Instruction deletion strength', default=0.7)
     parser.add_argument('--pmut', dest='pmut', type=float, help='Instruction mutation strength', default=0.7)
+    
+    # Trainer configuration
+    parser.add_argument('--generations', dest='num_generations', type=int, help='Number of generations over which evolution is performed', default=50)
+    parser.add_argument('--pop', dest='population_size', type=int, help='Learner population size', default=200)
+    parser.add_argument('--keep', dest='percent_keep', type=float, help='Percentage of surviving Learners', default=0.3)
+    parser.add_argument('--fast', dest='fast_mode', type=bool, help='Skip some re-evaluations', default=True)
+    parser.add_argument('--skips', dest='num_skips', type=int, help='Number of generations over which to skip re-evaluation', default=3)
+    parser.add_argument('--episodes', dest='num_eps_per_gen', type=int, help='Number of episodes over which an agent is evaluated each generation', default=3)
+    parser.add_argument('--verbose', dest='verbose', type=bool, help='Do print out info to the console during evolution', default=True)
+    parser.add_argument('--agent', dest='agent_save_name', type=str, help='Name under which to save the evolved agent', default="")
+    
+    # Environment configuration
     parser.add_argument('--env', dest='env', type=str, help='OpenAI environment', default="CartPole-v1")
     parser.add_argument('--statespace', dest='statespace', type=int, help='Length of flattened state space', default=4)
     args = parser.parse_args(arguments)
@@ -44,6 +58,16 @@ def run(arguments):
         p_add           = args.padd,
         p_del           = args.pdel,
         p_mut           = args.pmut)
+
+    ConfigureTrainer(
+        num_generations     = args.num_generations,
+        population_size     = args.poulation_size,
+        percent_keep        = args.percent_keep,
+        fast_mode           = args.fast_mode,
+        max_num_skips       = args.num_skips,
+        num_eps_per_gen     = args.num_eps_per_gen,
+        verbose             = args.verbose,,
+        agent_save_name     = args.agent_save_name)
 
     env = gym.make(args.env)
     trainer = Trainer(env)
